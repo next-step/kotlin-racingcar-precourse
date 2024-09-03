23f3952ef2cffd507902453516878ce3923eda77
solchan427 <qkrthfcks@gmail.com>
2024-09-03 18:17:09 +0900

    feat: 랜덤 숫자 뽑기 테스트 도출 및 기능 구현 - 2개 숫자 사이 랜덤 숫자 반환

---
+++ src/main/kotlin/RangeRandomNumberPicker.kt
@@ -0,0 +1,11 @@
+import kotlin.random.Random
+
+class RangeRandomNumberPicker(
+    private val start: Int,
+    private val end: Int
+) : RandomNumberPicker {
+
+    override fun pick(): Int {
+        return Random.nextInt(start, end + 1)
+    }
+}
