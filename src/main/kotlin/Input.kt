ac8f832991176bb7b04a6c80f3680e8fbcc8704d
solchan427 <qkrthfcks@gmail.com>
2024-09-03 20:50:29 +0900

    feat: Input, Output 기존 메서드 static으로 변경 및 인스턴스화 방지 로직 추가

--- src/main/kotlin/Input.kt
+++ src/main/kotlin/Input.kt
@@ -1,27 +1,30 @@
-class Input {
+class Input private constructor() {
 
-    fun readCarNames(): Set<CarName> {
-        return readInputWithRetry {
-            println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)")
-            readln().split(",")
-                .map { CarName(it.trim()) }
-                .toSet()
+    companion object {
+
+        fun readCarNames(): Set<CarName> {
+            return readInputWithRetry {
+                println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)")
+                readln().split(",")
+                    .map { CarName(it.trim()) }
+                    .toSet()
+            }
         }
-    }
 
-    fun readPlayCount(): Int {
-        return readInputWithRetry {
-            println("시도할 횟수는 몇회인가요?")
-            readln().toInt()
+        fun readPlayCount(): Int {
+            return readInputWithRetry {
+                println("시도할 횟수는 몇회인가요?")
+                readln().toInt()
+            }
         }
-    }
 
-    private fun <T> readInputWithRetry(action: () -> T): T {
-        return try {
-            action()
-        } catch (e: IllegalArgumentException) {
-            println("[ERROR] ".plus(e.message + "\n"))
-            readInputWithRetry(action)
+        private fun <T> readInputWithRetry(action: () -> T): T {
+            return try {
+                action()
+            } catch (e: IllegalArgumentException) {
+                println("[ERROR] ".plus(e.message + "\n"))
+                readInputWithRetry(action)
+            }
         }
     }
 }
