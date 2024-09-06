4536641672dae8b12e51d5e9ec2b3a0d257eeaac
sanghoon.lee <ilovejinjoo@gmail.com>
2024-09-06 19:29:35 +0900

    feat (): Implement input try num
    - Add inputTryNum function
    - Store car's name with RacingCar class's instance
    
    None

--- src/main/kotlin/racingcar.kt
+++ src/main/kotlin/racingcar.kt
@@ -1,29 +1,52 @@
 package com.example.randombox
 
 fun main() {
-    println("Input racingcar's name. (name can be devided by qutation(,)")
+    var nameList = ArrayList<RacingCar>()
+    var numOfTry = 0
 
-    var nameList = ArrayList<String>()
-
-    loop@while(true) {
+    loop1@while(true) {
+        println("Input racingcar's name. (name can be devided by qutation(,)")
         val names = inputNames().split(',')
-
         for (n in names) {
             if (n.length > 4) {
                 println("[ERROR] Invalid name, Please input again.")
                 nameList.clear()
-                continue@loop
+                continue@loop1
             } else {
-                nameList.add(n)
+                val car = RacingCar(n)
+                nameList.add(car)
             }
         }
         break
     }
+
+    loop2@while(true) {
+        println("How many times will you try? ")
+        numOfTry = inputTryNum()
+
+        if(numOfTry > 0) {
+            break;
+        } else {
+            println("[ERROR] Invalid number")
+            continue@loop2
+        }
+    }
 }
 
 fun inputNames() : String {
-    val names = readLine().toString()
+    val names = readLine()!!.trim().toString()
     return names
+}
+
+fun inputTryNum() : Int {
+    var tryNum = 0
+    try {
+        tryNum = readLine()!!.trim().toInt()
+    } catch ( e: NumberFormatException) {
+        return -1
+    }
+
+    return tryNum
 }
 
 class RacingCar (name : String ){
