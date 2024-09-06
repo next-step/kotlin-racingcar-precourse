f8c98228c61fd5df678284e9253fd2bb2b8954ad
powertay <powerms@hyundai.com>
2024-09-06 19:57:54 +0900

    :lipstick: 가

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,26 @@
 import domain.Car
+import domain.Cars
 import view.InputView
+import view.OutputView
+import java.util.*
 
 fun main() {
     val inputView = InputView()
+    val outputView = OutputView()
+
     val inputCarNames = inputView.inputCarNames()
-    val cars = inputCarNames
-        .map { Car(carName = it) }
-        .toList()
+    val cars = Cars(
+        inputCarNames.map { Car(carName = it) }.toList()
+    )
+
+    outputView.printResultHeader()
 
     val inputNumberOfAttempts = inputView.inputNumberOfAttempts()
+    repeat(inputNumberOfAttempts) {
+        cars.move(Random())
+        outputView.printCurrent(cars)
+    }
+
+    val winners = cars.winners()
+    outputView.printResultWinner(winners)
 }
---
+++ src/main/kotlin/view/OutputView.kt
@@ -0,0 +1,24 @@
+package view
+
+import domain.Cars
+
+class OutputView {
+    fun printCurrent(cars: Cars) {
+        cars.unmodifiableList.forEach {
+            val name = it.nameForPrint
+            val position = it.wherePosition
+            val display = "-".repeat(position)
+            println("$name : $display")
+        }
+        println()
+    }
+
+    fun printResultHeader() {
+        println("실행 결과")
+    }
+
+    fun printResultWinner(winners: Cars) {
+        val winnerNames = winners.unmodifiableList.joinToString(", ") { it.nameForPrint }
+        println("최종 우승자 : $winnerNames")
+    }
+}
