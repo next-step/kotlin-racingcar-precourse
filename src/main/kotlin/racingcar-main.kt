c3389d3088a30aadd380739ade45ed1d78a32ea6
kwak <bhkwak89@gmail.com>
2024-09-08 00:49:04 +0900

    feat(main) Implement main function for running racing car game

--- src/main/kotlin/racingcar-main.kt
+++ src/main/kotlin/racingcar-main.kt
@@ -1,2 +1,14 @@
-class `racingcar-main` {
+import controller.RacingController
+import model.RacingCarRepository
+import view.RacingCarView
+
+fun main() {
+
+    val racingCarView = RacingCarView()
+    val racingCarRepository = RacingCarRepository()
+    val racingCarController = RacingController(racingCarView, racingCarRepository)
+
+    racingCarController.initRacingCar()
+    racingCarController.playRacing()
+    racingCarController.finalWinners()
 }
