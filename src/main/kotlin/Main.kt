3575e6b5c2f83682c33803a7a8197bf66de39c64
UFO Hibachi Express <ufohibachi@gmail.com>
2024-09-08 19:03:44 +0900

    feat (): exception added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,36 +5,41 @@ fun main() {
     var carNames : List<String>;
     var raceTimes : Int;
     var cars : ArrayList<Car> = ArrayList<Car>();
-    var inputError : Boolean = false;
 
     // Input names
-    while (true) {
+    NameError@while (true) {
         println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         val command = readln();
-        var tempNames = command.split(",");
-        tempNames.forEach Break@{
-            if (it.length > 5) {
-                println("글자 수가 너무 커");
-                inputError = true;
-                return@Break;
+        var tempNames = command.trim().split(",");
+        for (name in tempNames) {
+            try {
+                if (name.length > 5) {
+                    throw IllegalArgumentException();
+                }
+            } catch (_: IllegalArgumentException) {
+                println("[ERROR] car name is too long...");
+                continue@NameError;
             }
         }
-        if (inputError != true) {
-            carNames = tempNames;
-            break;
-        } else {
-            inputError = false;
-        }
+        carNames = tempNames;
+        break;
     }
 
     // Input the numbers of times
-    while (true) {
+    NumberError@while (true) {
         println("시도할 횟수는 몇 회인가요?");
-        val tempNumber = readln().toIntOrNull();
+        val tempNumber = readln().trim().toIntOrNull();
 
-        if (tempNumber != null && tempNumber > 0) {
-            raceTimes = tempNumber;
-            break;
+        try {
+            if (tempNumber != null && tempNumber > 0) {
+                raceTimes = tempNumber;
+                break;
+            } else {
+                throw IllegalArgumentException();
+            }
+        } catch (_: IllegalArgumentException) {
+            println("[ERROR] input number is wrong...");
+            continue@NumberError;
         }
     }
 
@@ -47,24 +52,12 @@ fun main() {
     for (i: Int in 0..raceTimes - 1) {
         cars.forEach {
             it.drive();
-            print(it.name + " : " + "");
-            for (j: Int in 0..it.move - 1) {
-                print("-");
-            }
-            println();
+            RaceView.racingView(it.name, it.move);
         }
         println();
     }
 
     // Winners
     print("최종 우승자 : ");
-    cars.sortByDescending { it.move }
-    val bestMove = cars[0].move;
-    print(cars[0].name);
-    cars.forEach {
-        if (it.move == bestMove && it.name != cars[0].name) {
-            print(", " + it.name);
-        }
-    }
-    println();
+    RaceView.winnerView(cars);
 }
