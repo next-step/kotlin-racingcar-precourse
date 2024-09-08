60b9c41f71a799877065079ab456c95da2a9aa10
jaewon.jung <jgd0948@naver.com>
2024-09-08 13:15:32 +0900

    feat: MVC 구조 설정

---
+++ src/main/kotlin/RacingApplication.kt
@@ -0,0 +1,11 @@
+
+
+fun main() {
+    // 차량 input 받음
+    // controller 차량 등록 호출
+    // 몇회 전진 입력 받음
+    // controller 전진 호출
+    // 차량 전진 출력
+    // controller 우승자 계산 호출
+    // 우승자 출력
+}
---
+++ src/main/kotlin/controller/RacingController.kt
@@ -0,0 +1,20 @@
+package controller
+
+import model.Car
+import view.RacingView
+import view.WinnerView
+
+class RacingController {
+
+    fun registerCars(): List<Car> {
+        TODO()
+    }
+
+    fun racing(): RacingView {
+        TODO()
+    }
+
+    fun calculateWinner(): WinnerView {
+        TODO()
+    }
+}
