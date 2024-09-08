754fd78f2945664b73b4ee6afb1392ab5dfd2c3d
hengzizng <hengzizng@naver.com>
2024-09-09 01:24:45 +0900

    feat: 자동차 이동 구현

--- src/main/kotlin/racingcar/controller/RaceController.kt
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -1,14 +1,19 @@
 package racingcar.controller
 
+import racingcar.model.Car
 import racingcar.view.RaceView
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import kotlin.random.Random
 
 class RaceController() {
     private val raceView = RaceView()
 
     fun play() {
-        val carNameList = getCarNameList()
+        val carList = getCarNameList().map { Car(it) }
+
+        moveAllCar(carList)
+        raceView.printCarStatusList(carList)
     }
 
     private fun getCarNameList(): List<String> = with(BufferedReader(InputStreamReader(System.`in`))) {
@@ -20,5 +25,17 @@ class RaceController() {
         }
 
         return carNameList
+    }
+
+    private fun canMove(): Boolean {
+        return Random.nextInt(0, 10) >= 4
+    }
+
+    private fun moveAllCar(carList: List<Car>) {
+        carList.forEach { car: Car ->
+            if (canMove()) {
+                car.position++
+            }
+        }
     }
 }
--- src/main/kotlin/racingcar/view/RaceView.kt
+++ src/main/kotlin/racingcar/view/RaceView.kt
@@ -1,7 +1,15 @@
 package racingcar.view
 
+import racingcar.model.Car
+
 class RaceView {
     fun printCarNameGuide() {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    }
+
+    fun printCarStatusList(carList: List<Car>) {
+        carList.forEach { car: Car ->
+            println("${car.name} : ${"-".repeat(car.position)}")
+        }
     }
 }
