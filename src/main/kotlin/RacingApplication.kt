5085b0417fcf61e7573d39844cbabd68f75a691d
jaewon.jung <jgd0948@naver.com>
2024-09-08 14:02:28 +0900

    feat: main 차량 등록 로직 추가

--- src/main/kotlin/RacingApplication.kt
+++ src/main/kotlin/RacingApplication.kt
@@ -1,14 +1,30 @@
+import controller.RacingController
+import model.Car
 import view.RegisterCarView
 
+val racingCarController = RacingController()
 fun main() {
     // 차량 input 받음
     val registerCarView = RegisterCarView()
     registerCarView.printCarNameInputMessage()
     val carNames = registerCarView.scanCarNames()
+
     // controller 차량 등록 호출
+    val cars = registerCars(carNames)
+
     // 몇회 전진 입력 받음
     // controller 전진 호출
     // 차량 전진 출력
     // controller 우승자 계산 호출
     // 우승자 출력
+}
+
+private fun registerCars(carNames: String): List<Car> {
+    while (true) {
+        try {
+            return racingCarController.registerCars(carNames)
+        } catch (exception: IllegalArgumentException) {
+            println("[ERROR] ${exception.message}")
+        }
+    }
 }
