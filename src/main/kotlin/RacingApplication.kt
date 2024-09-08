261c404153148c1d89e00f0f816d8df0016b26fd
jaewon.jung <jgd0948@naver.com>
2024-09-08 14:37:18 +0900

    feat: 경주 라운드 입력 로직 추가

--- src/main/kotlin/RacingApplication.kt
+++ src/main/kotlin/RacingApplication.kt
@@ -1,13 +1,15 @@
 import controller.RacingController
 import model.Car
+import view.RacingView
 import view.RegisterCarView
-import view.View
 
 val racingCarController = RacingController()
 fun main() {
     val cars = registerCars()
 
     // 몇회 전진 입력 받음
+    val round = scanRound()
+
     // controller 전진 호출
     // 차량 전진 출력
     // controller 우승자 계산 호출
@@ -26,6 +28,18 @@ private fun registerCars(): List<Car> {
             return racingCarController.registerCars(carNames)
         } catch (exception: IllegalArgumentException) {
             registerCarView.printErrorMessage(exception)
+        }
+    }
+}
+
+private fun scanRound(): Int {
+    val racingView = RacingView()
+    while (true) {
+        try {
+            racingView.printRoundInputMessage()
+            return racingView.scanRound()
+        } catch (exception: NumberFormatException) {
+            racingView.printErrorMessage(exception)
         }
     }
 }
--- src/main/kotlin/view/RacingView.kt
+++ src/main/kotlin/view/RacingView.kt
@@ -1,4 +1,15 @@
 package view
 
 class RacingView : View {
+    companion object {
+        const val ROUND_INPUT_TEXT = "시도할 회수는 몇회인가요?"
+    }
+
+    fun printRoundInputMessage() {
+        println(ROUND_INPUT_TEXT)
+    }
+
+    fun scanRound(): Int {
+        return readln().toInt()
+    }
 }
