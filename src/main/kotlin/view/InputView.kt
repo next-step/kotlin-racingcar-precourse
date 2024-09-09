26e59c45b9c6e99939168cc7d2018cb4f536e965
powertay <powerms@hyundai.com>
2024-09-09 13:01:17 +0900

    refactor: InputView 예외 체크시 if 대신 require 사용

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -20,9 +20,8 @@ class InputView {
             println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
             val input = scanner.nextLine()
 
-            if (input.isBlank() || input.split(",").any { it.trim().isEmpty() }) {
-                throw IllegalArgumentException("[ERROR] 자동차 이름은 공백이나 빈 값이 될 수 없습니다.")
-            }
+            require(input.isNotBlank()) { "[ERROR] 자동차 이름은 공백이나 빈 값이 될 수 없습니다." }
+            require(input.split(",").all { it.isNotBlank() }) { "[ERROR] 자동차 이름은 공백이나 빈 값이 될 수 없습니다." }
 
             input.split(",").map { it.trim() }
         }
@@ -32,13 +31,10 @@ class InputView {
         return retryOnException {
             println("시도할 회수는 몇 회인가요?")
             val input = scanner.nextLine()
+            requireNotNull(input.toIntOrNull()) { "[ERROR] 시도할 회수는 숫자로 입력해 주세요." }
 
-            val numberOfAttempts = input.toIntOrNull()
-                ?: throw IllegalArgumentException("[ERROR] 시도할 회수는 숫자로 입력해 주세요.")
-
-            if (numberOfAttempts <= 0) {
-                throw IllegalArgumentException("[ERROR] 시도할 회수는 1회 이상이어야 합니다.")
-            }
+            val numberOfAttempts = input.toInt()
+            require(numberOfAttempts >= 1) { "[ERROR] 시도할 회수는 1회 이상이어야 합니다." }
 
             numberOfAttempts
         }
